dca28f38099dd5e0ac44b80836c138c1f51d56ec
elii-riim <55125098+elii-riim@users.noreply.github.com>
2019-11-20 09:44:22 -0500

    Add files via upload

---
+++ Lab4/Hello.py
@@ -0,0 +1,21 @@
+Python 3.8.0 (v3.8.0:fa919fdf25, Oct 14 2019, 10:23:27) 
+[Clang 6.0 (clang-600.0.57)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> #Sovathana Eli Rim
+>>> #November 20, 2019
+>>> #code by" Automate the boring stuff with Python
+>>> print('Hello World')
+Hello World
+>>> print('Whats is your name?')
+Whats is your name?
+>>> myName = input()
+print('It is good to meet you, ' + myName)
+>>> print('The length of your name is:')
+The length of your name is:
+>>> print(len(myName))
+42
+>>> print('Whats your age?')
+Whats your age?
+>>> myAge = input()
+print('Your will be ' + str(int(myAge) + 1) + ' in a year. ')
+>>> 
